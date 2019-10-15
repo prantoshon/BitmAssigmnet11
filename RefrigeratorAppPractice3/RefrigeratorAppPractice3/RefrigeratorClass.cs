b19b999a83fb040365595f175fa316c8d41743c4
prantoshon <prantoshon14@gmail.com>
2019-10-15 23:02:54 +0600

    done

---
+++ RefrigeratorAppPractice3/RefrigeratorAppPractice3/RefrigeratorClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefrigeratorAppPractice3
+{
+  
+    public class RefrigeratorClass
+    {
+        public double maxWeight { get; set; }
+
+        public  double itemNo { get; set; }
+        public  double  weight { get; set; }
+
+        public double CurretWeight()
+        {
+            return itemNo * weight;
+        }
+
+        public double Remain()
+        {
+            return maxWeight - CurretWeight();
+        }
+
+
+    }
+}
---
+++ RefrigeratorAppPractice3/RefrigeratorAppPractice3/RefrigeratorUi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RefrigeratorAppPractice3
+{
+    public partial class RefrigeratorUi : Form
+    {
+        public RefrigeratorUi()
+        {
+            InitializeComponent();
+        }
+
+        RefrigeratorClass refrigerator = new RefrigeratorClass();
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            refrigerator.maxWeight =Convert.ToDouble(maxWeightTakeTextBox.Text);
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            refrigerator.itemNo =Convert.ToDouble(itemTextBox.Text);
+            refrigerator.weight= Convert.ToDouble(weightTextBox.Text);
+            if (refrigerator.maxWeight > refrigerator.CurretWeight()) 
+            {
+                currentWeightTextBox.Text = refrigerator.CurretWeight().ToString();
+               remainingWeightTextBox.Text = refrigerator.Remain().ToString();
+            }
+            else
+            {
+                MessageBox.Show("It Become more than MAx Weight");
+                return;
+            }
+
+        }
+    }
+}
